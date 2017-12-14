ac87a15ae661244429d6f4f6d5b7cc4583875b3d
Hadi Panahi <panahi@gmail.com>
2017-12-14 01:32:59 -0600

    removing neos server code

--- ClusterWiseLinearRegression/src/execute/RunOriginalModel.java
+++ ClusterWiseLinearRegression/src/execute/RunOriginalModel.java
@@ -4,28 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
-import org.neos.client.NeosClient;
-import org.neos.client.NeosJobXml;
-
 import dataSource.DataSource;
 import ilog.concert.IloException;
 import models.OriginalModel;
 
 public class RunOriginalModel {
 
-	private static final String HOST="neos-server.org";
-	   private static final String PORT="3333";
-
 	
 	public static void main(String[] args) throws IOException, IloException {
 		// TODO Auto-generated method stub
 		
-		 NeosClient client = new NeosClient(HOST, PORT);
-		 /* create NeosJobXml object exJob with problem type nco for nonlinearly */
-	      /* constrained optimization, KNITRO for the solver, GAMS for the input */
-	      NeosJobXml job = new NeosJobXml("miqp", "CPLEX", "LP");
-	      
+		     
 	      
 	      
 		 
@@ -35,9 +24,7 @@ public class RunOriginalModel {
 //		dataSource.populateData();
 //		dataSource.dataReport();
 		originalModel.exportLP();
-		String modelString = null;
-		modelString = "C:\\Users\\hadipanahi\\git\\clusterwiseregression\\ClusterWiseLinearRegression\\model.lp";
-		job.addParam("model", modelString);
+		
 		//originalModel.solve();
 		
 		
